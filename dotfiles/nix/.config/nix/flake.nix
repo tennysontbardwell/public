04c8e149a5c43dc4742dfb4a3e44b9a534ae3376
Tennyson T Bardwell <tennysontaylorbardwell@gmail.com>
2025-03-24 21:12:54 -0500

    --wip-- [skip ci]

--- dotfiles/nix/.config/nix/flake.nix
+++ dotfiles/nix/.config/nix/flake.nix
@@ -84,65 +84,6 @@
         packages = myRPackages;
       };
 
-      myPythonEnv = pkgs.python3.withPackages (ps: with ps; with pkgs.python312Packages;
-        [
-          ### misc/basic
-          requests
-          lxml
-          beautifulsoup4
-          pypdf
-          tqdm
-          pip
-          aiohttp
-          scrapy
-
-          ### Data
-          matplotlib
-          numpy
-          pandas
-          polars
-          fastparquet
-          parquet
-          jupyter
-          jupyterlab
-          plotnine
-          scikit-learn
-          scikit-misc
-          spyder-kernels
-          scipy
-
-          ### AI
-          openai
-          parquet
-          torch
-          transformers
-          torchvision
-          torchaudio
-          huggingface-hub
-          # vllm
-
-          ## misc from vllm debugging
-          aioprometheus
-          fastapi
-          lm-format-enforcer
-          outlines
-          psutil
-          py-cpuinfo
-          pyarrow
-          pydantic
-          pyzmq
-          ray
-          sentencepiece
-          tiktoken
-          uvicorn
-          xformers
-          # prometheus-fastapi-instrumentator
-
-          ## not FOSS / not mac
-          # accelerate
-          # bitsandbytes
-      ]);
-
       commonPaths = with pkgs;
         [
           #### general tools
@@ -274,11 +215,6 @@
           nodejs
           yarn
           php
-          # python
-          myPythonEnv
-          uv
-          jetbrains.pycharm-community
-          pipx
           # R
           myREnv
           myRStudio
@@ -319,8 +255,10 @@
           # gitcheck-git   = multi git repo checker
           # percol
           # nnn            = file browser in cli
+        ]
+          ++ (./python.nix).paths
+      ;
 
-        ];
       commonEnv = pkgs.buildEnv {
         name = "home-packages";
         paths = commonPaths;
---
+++ dotfiles/nix/.config/nix/python.nix
@@ -0,0 +1,68 @@
+{ pkgs, ... }:
+{
+  myPythonEnv = pkgs.python3.withPackages (ps: with ps; with pkgs.python312Packages;
+    [
+      ### misc/basic
+      requests
+      lxml
+      beautifulsoup4
+      pypdf
+      tqdm
+      pip
+      aiohttp
+      scrapy
+
+      ### Data
+      matplotlib
+      numpy
+      pandas
+      polars
+      fastparquet
+      parquet
+      jupyter
+      jupyterlab
+      plotnine
+      scikit-learn
+      scikit-misc
+      spyder-kernels
+      scipy
+
+      ### AI
+      openai
+      parquet
+      torch
+      transformers
+      torchvision
+      torchaudio
+      huggingface-hub
+      # vllm
+
+      ## misc from vllm debugging
+      aioprometheus
+      fastapi
+      lm-format-enforcer
+      outlines
+      psutil
+      py-cpuinfo
+      pyarrow
+      pydantic
+      pyzmq
+      ray
+      sentencepiece
+      tiktoken
+      uvicorn
+      xformers
+      # prometheus-fastapi-instrumentator
+
+      ## not FOSS / not mac
+      # accelerate
+      # bitsandbytes
+  ]);
+
+  paths = with pkgs; [
+    myPythonEnv
+    uv
+    jetbrains.pycharm-community
+    pipx
+  ];
+}

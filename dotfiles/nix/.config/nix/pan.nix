61a3a734a5a372d3de30f11153737def86da6540
Tennyson T Bardwell <tennysontaylorbardwell@gmail.com>
2025-08-21 20:15:45 +0000

    --wip-- [skip ci]

--- dotfiles/nix/.config/nix/pan.nix
+++ dotfiles/nix/.config/nix/pan.nix
@@ -101,12 +101,14 @@ in
 #    }
 #  '';
 
-
   services.kubernetes = {
     roles = ["master" "node"];
-    masterAddress = "pan.tennysontbardwell.com";
-    apiserverAddress = "https://pan.tennysontbardwell.com:6443";
-    kubelet.kubeconfig.server = "https://pan.tennysontbardwell.com:6443";
+    # masterAddress = "pan.tennysontbardwell.com";
+    masterAddress = "pan";
+    # apiserverAddress = "https://pan.tennysontbardwell.com:6443";
+    apiserverAddress = "https://pan:6443";
+    # kubelet.kubeconfig.server = "https://pan.tennysontbardwell.com:6443";
+    kubelet.kubeconfig.server = "https://pan:6443";
     # easyCerts = true;
     apiserver = {
       securePort = 6443;
@@ -169,6 +171,7 @@ in
     imagemagick
     graphviz
     nixos-anywhere
+    openssl
 
     jellyfin
     tailscale
@@ -183,7 +186,6 @@ in
     ranger
     h2o
 
-    kubernetes
     cri-tools
   ])
     ++ packages.common_paths {

65c82bca75e8ed3f5c756d45a94ed8025094301d
Tennyson T Bardwell <tennysontaylorbardwell@gmail.com>
2025-10-14 21:47:47 -0500

    nix: launch agents for isync and my firefox extension server

--- dotfiles/nix/.config/nix/onyx-config.nix
+++ dotfiles/nix/.config/nix/onyx-config.nix
@@ -3,6 +3,25 @@ let
   system = "aarch64-darwin";
   packages = (import ./packages.nix { inherit lib pkgs; });
   common_paths = packages.common_paths { inherit system pkgs; };
+
+  mboxSync = mbox: freq: {
+    serviceConfig = {
+      Label = "org.tennyson.mbsync-${mbox}";
+      UserName = "tennyson";
+      StartInterval = freq; # every 1m
+      RunAtLoad = true;
+      WorkingDirectory = "/Users/tennyson";
+      ProgramArguments = [
+        "${pkgs.isync}/bin/mbsync"
+        "${mbox}"
+      ];
+      StandardOutPath = "/Users/tennyson/.local/var/log/mbsync-${mbox}.out";
+      StandardErrorPath = "/Users/tennyson/.local/var/log/mbsync-${mbox}.error.log";
+      EnvironmentVariables = {
+        PATH = "${pkgs.sops}/bin:${pkgs.gnupg}/bin:/usr/bin:/bin";
+      };
+    };
+  };
 in
 {
   # see https://nix-darwin.github.io/nix-darwin/manual/index.html#opt-homebrew.masApps
@@ -45,6 +64,34 @@ in
       host  all       all     127.0.0.1/32 trust
     '';
   };
+
+  launchd.user.agents.firefox-server =
+    let
+      python = pkgs.python3.withPackages (
+        ps: with ps; [
+          websockets
+          aiohttp
+        ]
+      );
+    in
+    {
+      serviceConfig = {
+        Label = "com.tennysonb.firefox-server";
+        ProgramArguments = [
+          "${python}/bin/python3"
+          "/Users/tennyson/repos/tennysontbardwell/misc-projects/firefox/my-extension/server.py"
+        ];
+        KeepAlive = true;
+        RunAtLoad = true;
+        StandardOutPath = "/Users/tennyson/.local/var/log/firefox-server.log";
+        StandardErrorPath = "/Users/tennyson/.local/var/log/firefox-server.error.log";
+      };
+    };
+
+  launchd.daemons.mbsync-fastmail = mboxSync "fastmail" 60;
+  launchd.daemons.mbsync-gmail-inbox = mboxSync "gmail-inbox" 60;
+  launchd.daemons.mbsync-allmail = mboxSync "gmail-allmail" (3600 * 5);
+  launchd.daemons.mbsync-gmail = mboxSync "gmail" (3600 * 1);
 
   security.pam.services.sudo_local.touchIdAuth = true;
   system = {
